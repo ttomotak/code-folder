88e10ba15e4d16876e4ac6edaf0f1343942dce46
ttomotak <65560103+ttomotak@users.noreply.github.com>
2020-06-12 17:18:45 +0900

    コメントの追記
    コメントの追記，修正

--- aws_news_scraping.py
+++ aws_news_scraping.py
@@ -37,7 +37,7 @@ if __name__ == '__main__':
     data_col = ["information1", "information2", "information3"]
 
     #取得ページ分ループする.例では3ページ分取得する
-    for _ in range(21):
+    for _ in range(3):
         res = requests.get(dynamic_url)
         res.raise_for_status()
 
@@ -46,12 +46,13 @@ if __name__ == '__main__':
 
         #「←Older Posts」ボタンと「Newer Posts→」ボタンのリンクから次ページへのリンクを取得する
         #最初のページでは「←Older Posts」ボタンから次ページのリンクを取得する
-        #2ページ目以降では「Newer Posts→」ボタンから次ページのリンクを取得する
+        #2ページ目以降では末尾から2つ目にある「←Older Posts」ボタンから次ページのリンクを取得する
+        #2ページ目以降では「Newer Posts→」ボタンが最後尾になる 例: (blog-btn-a)[-1]
         next_page = html.find_all("a",attrs={"class": "blog-btn-a"})[-1].get("href") if flag==1 else html.find_all("a",attrs={"class": "blog-btn-a"})[-2].get("href")
         flag = 0
 
         #ページ内の記事数分ループする
-        #AWS公式ブログでは1ページに10記事あるため，10回ループする．
+        #例： AWS公式ブログでは1ページに10記事あるため，10回ループする．
         for i in range(len(detail_url_list)):
             #取得記事を設定
             res2 = requests.get(urljoin(base_url, detail_url_list[i].a.get("href")))
@@ -62,17 +63,24 @@ if __name__ == '__main__':
             # 抜き出す情報に合わせて抽出するタグの変更
             #タイトル
             information1 = html2.title.string
-            #本文，htmlタグ除去，文章を要約
+            #本文
             information2 = html2.find_all('p')
+            #htmlタグ除去
             information2 = strip_tags(information2)
+            #文章を要約
             information2 = sum_text(information2)
+            #不要な改行 括弧[] カンマを取り除く
+            #reを使って改修したい
             information2 = information2.replace('\n','')
             information2 = information2.replace('[','')
             information2 = information2.replace(']','')
             information2 = information2.replace(',','')
             #発行日付
             information3 = html2.find_all('time')
+            #htmlタグ除去
             information3 = strip_tags(information3)
+            #不要な改行 括弧[] カンマを取り除く
+            #reを使って改修したい
             information3 = information3.replace('[','')
             information3 = information3.replace(']','')
             information3 = information3.replace(',','')
